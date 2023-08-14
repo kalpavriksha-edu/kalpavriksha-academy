c51186b0f3e97f57314204cc4340f9526e67c6a8
Shashi Kumar <shashisharma50102@gmail.com>
2023-08-14 15:18:27 +0530

    Remove extra spaces

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -3,12 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db_config = {
-    HOST: process.env.DB_HOST ,
-    USER: process.env.DB_USER ,
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
-    DATABASE: process.env.DB_NAME ,
-}
-
-
-
-export default db_config
+    DATABASE: process.env.DB_NAME,
+};
+export default  db_config;
