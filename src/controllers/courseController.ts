435fbb899f7f6032ffcbe22fbe5c61e7d98b90aa
Shashi Kumar <shashisharma50102@gmail.com>
2023-08-17 04:07:57 +0530

    Updated and modified with logger.

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -1,74 +1,65 @@
 import { Request, Response } from 'express';
-import Course from "../model/course.model";
+import Course from "../model/courseModel";
+import CourseService from '../service/courseService';
+import { responseGenerator } from '../utility/responseGenerator';
+import logger from '../utility/logger';
+
+const courseService = new CourseService();
 
 class CourseController {
-    public static async get() {
+    public async getCourses(req: Request, res: Response) {
         try {
-            const course = await Course.findAll();
-            return course;
-        } catch (error: any) {
-            console.error('An error occurred:', error.message);
-            console.error('Stack trace:', error.stack);
-            return { "error": 'Internal server error' };
+            const courses = await courseService.getCourses();
+            return responseGenerator.getSuccessResponse(res, 'Courses Fetched Successfully', courses);
+        } catch (error) {
+            logger.error('An error occurred:', error);
+            return responseGenerator.getErrorResponse(res, 500);
         }
     };
 
-    public static async getById(id: number) {
+    public async getCourseById(req: Request, res: Response) {
+        const id: number = req.params.id;
         try {
-            const course = await Course.findByPk(id);
-            return course;
-        } catch (error: any) {
-            console.error('An error occurred:', error.message);
-            console.error('Stack trace:', error.stack);
-            return { "error": 'Internal server error' };
+            const course = await courseService.getCourseById(id);
+            return responseGenerator.getSuccessResponse(res, 'Course Fetched Successfully', course);
+        } catch (error) {
+            logger.error('An error occurred:', error);
+            return responseGenerator.getErrorResponse(res, 404);
         }
     };
 
-    public static async create(body: any) {
-        const { name, description, created_by } = body;
+    public async createCourse(req: Request, res: Response) {
+        const { name, description, created_by } = req.body;
         try {
-            const course = await Course.create({
-                name: String(name),
-                description: String(description),
-                created_by: String(created_by)
-            });
-            return { msg: "Successfully Created" };
+            const course = await courseService.createCourse(name, description, created_by);
+            return responseGenerator.getSuccessResponse(res, 'Course Successfully Created', course);
         } catch (error: any) {
-            console.error("An error occurred:", error.message);
-            console.error("Stack trace:", error.stack);
-            return { error: "Internal server error" };
+            logger.error('An error occurred:', error);
+            return responseGenerator.getErrorResponse(res, 500);
         }
     };
 
-    public static async delete(id: number) {
+    public async deleteCourse(req: Request, res: Response) {
+        const id: number = req.params.id;
         try {
-            await Course.destroy({
-                where: {
-                    course_id: id
-                }
-            });
-            return { msg: "Successfully Deleted" }
-        } catch (error: any) {
-            console.error('An error occurred:', error.message);
-            console.error('Stack trace:', error.stack);
-            return { "error": 'Internal server error' };
+            await courseService.deleteCourse(id);
+            return responseGenerator.getSuccessResponse(res, 'Course Successfully Deleted');
+        } catch (error) {
+            logger.error('An error occurred:', error);
+            return responseGenerator.getErrorResponse(res, 404);
         }
     };
 
-    public static async update(body: any, id: number) {
+    public async updateCourse(req: Request, res: Response) {
+        const id: number = req.params.id;
         try {
-            const course = await Course.update(body, {
-                where: {
-                    course_id: id,
-                },
-            });
-            return { msg: "Updated Successfully" };
-        } catch (error: any) {
-            console.error('An error occurred:', error.message);
-            console.error('Stack trace:', error.stack);
-            return { "error": 'Internal server error' };
+            await courseService.updateCourse(id, req.body);
+            return responseGenerator.getSuccessResponse(res, 'Course Updated Successfully');
+        } catch (error) {
+            logger.error('An error occurred:', error);
+            return responseGenerator.getErrorResponse(res, 404);
         }
     };
 }
 
-export default CourseController;
+export const courseController = new CourseController();
