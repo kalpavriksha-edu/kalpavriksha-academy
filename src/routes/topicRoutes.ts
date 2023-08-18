7d6c81fb75a3857c4eee563dce20e9ec41b35d87
Komalgore57 <gorekomal576@gmail.com>
2023-08-18 21:34:17 +0530

    Create Route for topicRoutes

---
+++ src/routes/topicRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { topicController } from "../controllers/topicController";
+
+const topicRouter = Router();
+
+topicRouter.get('/topics', topicController.getTopics);
+topicRouter.get('/topic/:id', topicController.getTopicById);
+topicRouter.get('/topic/course/:id', topicController.getTopicByCouser);
+topicRouter.post('/topic', topicController.createTopic);
+topicRouter.patch('/topic/:id', topicController.updatetTopic);
+topicRouter.delete('/topic/:id', topicController.deleteTopic);
+
+module.exports = topicRouter;
