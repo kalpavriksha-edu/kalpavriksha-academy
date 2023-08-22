7809358311cb06d16bc4bb2cc656913c026fa3a0
Shashi Kumar <shashisharma50102@gmail.com>
2023-08-22 09:48:42 +0530

    Create a Server class and refactor code

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 import router = require("./routes/courseRoutes");
-import { Request, Response } from 'express';
-import  loggerManager  from "./utility/logger";
-const errorEnums  = require('./constants/errorConstants');
+import { Request, Response, Express } from 'express';
+import loggerManager from "./utility/logger";
+const errorEnums = require('./constants/errorConstants');
 import { port } from "./config/dbConfig";
+import Database from './db/dbConnection';
+import { Logger } from "winston";
 
-const PORT = port.PORT;
-const app = express();
-
-import { database } from './db/dbConnection';
-const logger = loggerManager.getLogger();
-
-(async()=> {
-  try {
-    app.listen(PORT, () => {
-      console.log("Server is running at port..." + PORT);
-    });
-    await database.connect();
-    await database.syncDatabase();
-  } catch (error) {
-    logger.error(error);
+class Server {
+  private PORT: string;
+  private app: Express;
+  logger: Logger;
+  database: Database;
+  constructor() {
+    this.PORT = port.PORT;
+    this.logger = loggerManager.getLogger();
+    this.app = express();
+    this.database = new Database();
   }
-})();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', router);
-app.use((req: Request, res: Response) => {
-  res.status(404).json({ error: errorEnums.ERR_URL });
-});
+
+  async start() {
+    try {
+      this.app.listen(this.PORT, () => {
+        console.log("Server is running at port..." + this.PORT);
+      });
+      await this.database.connect();
+      await this.database.syncDatabase();
+    } catch (error) {
+      this.logger.error(error);
+    }
+
+    this.setupMiddleware();
+    this.setupRoutes();
+    this.setup404Handler();
+  }
+
+  setupMiddleware() {
+    this.app.use(express.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  setupRoutes() {
+    this.app.use('/', router);
+  }
+
+  setup404Handler() {
+    this.app.use((req: Request, res: Response) => {
+      res.status(404).json({ error: errorEnums.ERR_URL });
+    });
+  }
+}
+
+const server = new Server();
+server.start();
