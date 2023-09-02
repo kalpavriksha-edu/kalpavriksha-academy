dc73e9d6fc834c9b8aed235ed0f5ce450a9dadb2
pv0910 <pjv987@gmail.com>
2023-09-02 22:11:19 +0530

    updated dbConfig.ts for login-Register

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -6,7 +6,13 @@ export const dbConfig = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
-    DATABASE: process.env.DB_NAME
+    DATABASE: process.env.DB_NAME,
+    JWT_SECRET: process.env.JWT_SECRET
+};
+
+export const SMTP_CONFIG = {
+SMTP_MAIL: process.env.SMTP_MAIL,
+SMTP_PASSWORD: process.env.SMTP_PASSWORD
 };
 
 export const port = {
