c2bcb0f44bfe2915658819c8fc0f8890b07d036a
Komalgore57 <55106162+Komalgore57@users.noreply.github.com>
2023-09-02 14:44:08 +0530

    Topic model (#10)
    * Created Topic Model Class
    
    * Created Topic Model Controller
    
    * Created topic Model Services
    
    * Created topic model API routes
    
    * changed success Constant COURSE_FETCHED to FETCHED
    
    * changed success constant COURSE_CREATED to CREATED
    
    * Change success constant COURSE_FETCH to FETCH
    
    * Change success constant COURSE_CREATED to CREATED
    
    * minor changes

---
+++ src/model/topicModel.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+import Course from '../model/courseModel'
+import Database from "../db/dbConnection"
+
+const database = new Database();
+database.connect()
+database.syncDatabase()
+const sequelize=database.getSequelizeInstance()
+const Topic = sequelize.define('Topic', {
+
+    topic_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    video_link: {
+        type: DataTypes.STRING,
+    },
+    created_by: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    modelName: 'Course',
+    timestamps: true
+});
+
+Course.hasMany(Topic, { foreignKey: 'course_id' });
+
+export default Topic;
