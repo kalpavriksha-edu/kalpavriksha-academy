3b0ef681ebbea561dac64ba53a95d69ba10ebe53
Shashi Kumar <shashisharma50102@gmail.com>
2023-09-04 15:40:13 +0530

    Added type conversion to number for 'id' parameter in the request

--- src/controllers/assignmentController.ts
+++ src/controllers/assignmentController.ts
@@ -3,7 +3,6 @@ import { responseGenerator } from "../utility/responseGenerator"
 import AssignmentService from "../service/assignmentService"
 import loggerManager from "../utility/logger"
 import successEnums from "../constants/successConstant"
-import { log } from "winston"
 
 const logger = loggerManager.getLogger();
 const assignmentService = new AssignmentService();
@@ -12,7 +11,7 @@ class AssignmentController {
     public async getAssignments(req: Request, res: Response) {
         try {
             const assignments = await assignmentService.getAssignments();
-            return responseGenerator.getSuccessResponse(res, successEnums.COURSE_FETCHED, assignments);
+            return responseGenerator.getSuccessResponse(res, successEnums.FETCHED, assignments);
         } catch (error) {
             logger.error(error);
             return responseGenerator.getErrorResponse(res, 500);
@@ -20,10 +19,10 @@ class AssignmentController {
     }
 
     public async getAssignmentById(req: Request, res: Response) {
-        const id: number = req.params.id;
+        const id: number = Number(req.params.id);
         try {
             const assignment = await assignmentService.getAssignmentById(id);
-            return responseGenerator.getSuccessResponse(res, successEnums.COURSE_FETCHED, assignment);
+            return responseGenerator.getSuccessResponse(res, successEnums.FETCHED, assignment);
         } catch (error) {
             logger.error(error);
             return responseGenerator.getErrorResponse(res, 404);
@@ -34,7 +33,7 @@ class AssignmentController {
         const { name, description, max_score, created_by, topic_id } = req.body;
         try {
             const assignment = await assignmentService.createAssignment(name, description, max_score, created_by, topic_id);
-            return responseGenerator.getSuccessResponse(res, successEnums.COURSE_CREATED, assignment);
+            return responseGenerator.getSuccessResponse(res, successEnums.CREATED, assignment);
         } catch (error: any) {
             logger.error(error);
             return responseGenerator.getErrorResponse(res, 500);
@@ -42,7 +41,7 @@ class AssignmentController {
     }
 
     public async deleteAssignment(req: Request, res: Response) {
-        const id: number = req.params.id;
+        const id: number = Number(req.params.id);
         try {
             await assignmentService.deleteAssignment(id);
             return responseGenerator.getSuccessResponse(res, successEnums.DELETE_SUCCESS);
@@ -53,7 +52,7 @@ class AssignmentController {
     }
 
     public async updateAssignment(req: Request, res: Response) {
-        const id: number = req.params.id;
+        const id: number = Number(req.params.id);
         try {
             await assignmentService.updateAssignment(id, req.body);
             return responseGenerator.getSuccessResponse(res, successEnums.UPDATE_SUCCESS);
