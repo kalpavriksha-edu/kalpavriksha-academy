826f0717abffd0d379d6ecb2211c70b14d6470e4
DeveloperYogesh07 <yogiupadhyay07@gmail.com>
2023-09-05 09:55:07 +0530

    Added getUserById in ProtectedRoutes , Added RoleValidation to routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import authorization from "../authentication/tokenValidation";
+import Authorization from "../authentication/auth";
 import { UserController } from "../controllers/userController";
+import Roles from "../config/roles";
 
 const userController = new UserController();
+const authorization = new Authorization();
 export default class Routes {
   private router: Router = Router();
 
@@ -10,18 +12,17 @@ export default class Routes {
     const publicRouter = Router();
     publicRouter.post("/user", userController.createNewUser);
     publicRouter.post("/user/login", userController.userLogin);
-    publicRouter.get("/user/:id",userController.getUserById);
     return publicRouter;
   }
 
-   protectedRoutes(): Router {
+  protectedRoutes(): Router {
     const protectedRouter = Router();
-    protectedRouter.use(authorization);
-    protectedRouter.get("/users", userController.getAllUsers);
+    protectedRouter.use(authorization.tokenValidation);
+    protectedRouter.use(authorization.roleValidation(Roles.roles.Admin));
     protectedRouter.patch("/user/:id", userController.updateUserById);
+    protectedRouter.get("/users", userController.getAllUsers);
+    protectedRouter.get("/user/:id", userController.getUserById);
     protectedRouter.delete("/user/:id", userController.deleteUserById);
     return protectedRouter;
   }
-
-   
 }
