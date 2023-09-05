19d524a41a43f94b9be8a70c7bced011ca760794
DeveloperYogesh07 <yogiupadhyay07@gmail.com>
2023-09-05 10:04:58 +0530

    added getError function . it will take a error and will return corsponding error and error code

--- src/utility/responseGenerator.ts
+++ src/utility/responseGenerator.ts
@@ -1,4 +1,5 @@
 import { type Response } from "express"
+import errorConstant from "../constants/errorConstants";
 
 type Messages = Record<number, string>
 
@@ -8,6 +9,7 @@ class ResponseGenerator {
         400: "Bad Request: The request was malformed.",
         404: "Resource not found",
         500: "Internal Server Error",
+        403: "Invalid Arguments"
     }
     public getErrorResponse(res: Response, errorCode: number) {
         const response = {
@@ -16,6 +18,19 @@ class ResponseGenerator {
         }
         return res.json(response);
     };
+
+    public getError(error: any) {
+        if (error.message.includes('Illegal arguments')) {
+            return {message:"Illegal arguments",code:403};
+          } 
+        if (error.message.includes('Resources Not Found')) {
+            return {message:"Resources Not Found",code:404};
+          } 
+        if (error.message.includes('data and hash arguments required')) {
+            return {message:" data and hash arguments required",code:400};
+          } 
+          return {message:"Internal server Error",code:500};
+      }
 
     public getSuccessResponse(res: Response, message: { message: string }, data?: any) {
         const response = { message, data };
