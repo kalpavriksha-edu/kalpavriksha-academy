5b1f0e1dffbfdc7795e96146e695eb17cc522df7
pv0910 <pjv987@gmail.com>
2023-10-04 15:06:16 +0530

    add routes for google auth

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -10,5 +10,8 @@ router.post('/login', loginValidation, loginController.login);
 
 router.get('/auth/google', loginController.googleAuth); 
 router.get('/auth/google/callback', loginController.googleAuthCallback); 
+router.get('/failed', loginController.failed); 
+router.get('/success', loginController.success); 
+router.get('/logout',loginController.logout);
 
 export default router;
